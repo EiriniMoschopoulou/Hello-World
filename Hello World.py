34136129aec5a1a1648b738b54f26834aaa1bf62
Eirini Moschopoulou <eirhnh.moschop@gmail.com>
2017-11-22 12:26:12 +0200

    some functions and modules added

--- Hello World.py
+++ Hello World.py
@@ -3,4 +3,19 @@ welcome=["Hello", "world"]
 for n in welcome :
     print(n)
 
-    
+
+
+
+
+#Modules=functions poy tis grafoyme se ksexwristo file kai mporei na tis xrhsimopoihsei kathe python file sto idio directory
+import Thefunctions
+Thefunctions.function1()
+
+
+#file random: periexei oles tis basikes synarthseis tou mycharm
+
+
+import random
+
+y=random._log(55,2)
+print(y)
